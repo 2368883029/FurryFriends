dfe3689eb626989be9bfb87610a566afc2d45096
2368883029 <95886018+2368883029@users.noreply.github.com>
2023-11-12 17:21:11 -0500

    Update Notifications

--- P2/notifications/urls.py
+++ P2/notifications/urls.py
@@ -3,10 +3,9 @@ from . import views
 from rest_framework.routers import DefaultRouter
 
 router = DefaultRouter()
-router.register('id', views.NotificationManageViewSet, basename='notifications')
+router.register('', views.NotificationManageViewSet, basename='notifications')
 
 urlpatterns = [
-    path('', views.NotificationCreateView.as_view(), name='create-notifications'),
-    path('page/<int:page>/status/<int:read>/', views.NotificationListView.as_view(), name='list-notifications'),
+    # path('page/<int:page>/status/<int:read>/', views.NotificationListView.as_view(), name='list-notifications'),
     path('', include(router.urls)),
 ]
--- P2/notifications/views.py
+++ P2/notifications/views.py
@@ -1,3 +1,4 @@
+from django.forms import ValidationError
 from django.shortcuts import get_object_or_404
 from rest_framework.generics import CreateAPIView, ListAPIView, DestroyAPIView, RetrieveAPIView
 from .models import Notifications
@@ -7,23 +8,58 @@ from rest_framework.exceptions import PermissionDenied
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.viewsets import ModelViewSet
 from rest_framework import status
-
-
-class NotificationCreateView(CreateAPIView):
+        
+class NotificationManageViewSet(ModelViewSet):
     serializer_class = NotificationSerializer
     permission_classes = [IsAuthenticated]
 
-    def get_queryset(self):
-        return Notifications.objects.all()
+    def retrieve(self, request, *args, **kwargs):
+        instance = self.get_object()
     
-class NotificationListView(ListAPIView):
-    serializer_class = NotificationSerializer
-    permission_classes = [IsAuthenticated]
+        if instance.owner != request.user:
+            raise PermissionDenied('No permission')
+        
+        serializer = self.get_serializer(instance)
+        return Response(serializer.data)
 
-    def get_queryset(self):
-        current_user = self.request.user
-        page = self.kwargs.get('page')
-        read = self.kwargs.get('read')
+    def destroy(self, request, *args, **kwargs):
+        instance = self.get_object()
+
+        if instance.owner != request.user:
+            raise PermissionDenied('No permission')
+        
+        self.perform_destroy(instance)
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+    def partial_update(self, request, *args, **kwargs):
+        instance = self.get_object()
+        
+        if instance.owner != request.user:
+            raise PermissionDenied('No permission')
+        
+        instance.read = True
+        instance.save()
+
+        serializer = self.get_serializer(instance)
+        return Response(serializer.data)
+    
+    def list(self, request, *args, **kwargs):
+        try:
+            sort = int(request.query_params.get('sort', 0))
+            read = int(request.query_params.get('read', 0))
+            page = int(request.query_params.get('page', 1))
+        except ValueError:
+            raise ValidationError("Invalid input. Parameters 'sort', 'read', and 'page' must be integers.")
+
+        if sort not in [0, 1]:
+            raise ValidationError("Invalid value for 'sort'.")
+
+        if read not in [0, 1]:
+            raise ValidationError("Invalid value for 'read'.")
+
+        if page < 1:
+            raise ValidationError("Page number must be a positive integer.")
+        current_user = request.user
         page_size = 10
         start = (page - 1) * page_size
         end = page * page_size
@@ -33,31 +69,15 @@ class NotificationListView(ListAPIView):
             is_read = False
         else:
             is_read = True
-
-        return Notifications.objects.filter(read=is_read).filter(owner=current_user)[start:end]
         
+        list_ordered = Notifications.objects
+        if (sort):
+            list_ordered = list_ordered.order_by('-creation_time')
+        list_info = list_ordered.filter(read=is_read).filter(owner=current_user)[start:end]
+        serializer = self.get_serializer(list_info, many=True)
 
-class NotificationManageViewSet(ModelViewSet):
-    serializer_class = NotificationSerializer
-    permission_classes = [IsAuthenticated]
-
-    def retrieve(self, request, *args, **kwargs):
-        instance = self.get_object()
-    
-        if instance.owner != request.user:
-            return PermissionDenied('No permission')
-        
-        serializer = self.get_serializer(instance)
         return Response(serializer.data)
-
-    def destroy(self, request, *args, **kwargs):
-        instance = self.get_object()
-
-        if instance.owner != request.user:
-            return PermissionDenied('No permission')
         
-        self.perform_destroy(instance)
-        return Response(status=status.HTTP_204_NO_CONTENT)
 
     def get_queryset(self):
         return Notifications.objects.all() 
